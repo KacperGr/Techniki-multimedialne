4207b1f04bc3fb90874d21365f55d1c98678b3be
KacperGr <kacper.gruszczynski@wp.pl>
2016-05-11 12:57:27 +0200

    Update Modul_Open_CL.cpp
    dodana funkcja tworząca wskazniki na funkcje

--- Tutorial04/Modul_Open_CL.cpp
+++ Tutorial04/Modul_Open_CL.cpp
@@ -1,28 +1,47 @@
 #include "Modul_Open_CL.h"
 
+//wskaÅºniki na funkcje rozszerzeÅ„
+clCreateFromD3D11Texture2DKHR_fn   ptrToFunction_clCreateFromD3D11Texture2DKHR = NULL;
+clEnqueueAcquireD3D11ObjectsKHR_fn ptrToFunction_clEnqueueAcquireD3D11ObjectsKHR = NULL;
+clEnqueueReleaseD3D11ObjectsKHR_fn ptrToFunction_clEnqueueReleaseD3D11ObjectsKHR = NULL;
+
+void GetDX11SharingFunctionsFromCL()
+{
+
+	ptrToFunction_clCreateFromD3D11Texture2DKHR = (clCreateFromD3D11Texture2DKHR_fn)clGetExtensionFunctionAddressForPlatform(g_platformToUse, "clCreateFromD3D11Texture2DKHR");
+	if(ptrToFunction_clCreateFromD3D11Texture2DKHR == NULL)throw OpenClError{"Nieudane utworzenie wskaxnika na funckje"};
+	
+	ptrToFunction_clEnqueueAcquireD3D11ObjectsKHR = (clEnqueueAcquireD3D11ObjectsKHR_fn)clGetExtensionFunctionAddressForPlatform(g_platformToUse, "clEnqueueAcquireD3D11ObjectsKHR");
+	if(ptrToFunction_clEnqueueAcquireD3D11ObjectsKHR == NULL)throw OpenClError{"Nieudane utworzenie wskaxnika na funckje"};
+	
+	ptrToFunction_clEnqueueReleaseD3D11ObjectsKHR = (clEnqueueReleaseD3D11ObjectsKHR_fn)clGetExtensionFunctionAddressForPlatform(g_platformToUse, "clEnqueueReleaseD3D11ObjectsKHR");
+	if(ptrToFunction_clEnqueueReleaseD3D11ObjectsKHR == NULL)throw OpenClError{"Nieudane utworzenie wskaxnika na funckje"};
+
+}
+
 void system1(ID3D11Buffer* DirectXBuffer, const char* kernel_name)
 {
-	//tworzenie kontekstu obliczeñ dla danej platformy i urz¹dzenia obliczeniowego
+	//tworzenie kontekstu obliczeÃ± dla danej platformy i urzÂ¹dzenia obliczeniowego
 	cl_platform_id  platform;
 	cl_device_id  devices;
 	cl_int errcode;
 	//Bufor ramki
 	cl_mem Buffer;
 	errcode=clGetPlatformIDs(1, &platform, NULL);
-	//zg³oszenie wyj¹tku w przypadku niepowodzenia
+	//zgÂ³oszenie wyjÂ¹tku w przypadku niepowodzenia
 	if (platform == 0 || errcode != CL_SUCCESS)throw OpenCLError{ "Blad - Brak dostepnych platform OpenCL" };
 	clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &devices, NULL);
 	cl_context context = clCreateContext(0, 1, &devices,NULL, NULL, NULL);
 	
-	//tworzenie kolejki poleceñ
+	//tworzenie kolejki poleceÃ±
 	cl_command_queue  cmdQueue = clCreateCommandQueue(context, devices, 0, &errcode);
 	
 	size_t kernelLength;
 
-	//za³adowanie programu z pliku podanego jako argument funkcji
+	//zaÂ³adowanie programu z pliku podanego jako argument funkcji
 	char* programSource = loadProgSource(kernel_name, "", &kernelLength);
 
-	//struktura opisuj¹ca bufor
+	//struktura opisujÂ¹ca bufor
 	D3D11_BUFFER_DESC buffor_desc;
 
 	DirectXBuffer->GetDesc(&buffor_desc);
